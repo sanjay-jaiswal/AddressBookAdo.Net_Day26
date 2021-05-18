6f31b0132c758c0890886821383bb4867d222580
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-05-18 20:18:25 +0530

    [Sanju] ADD : Create table and insert contacts into table.

--- AddressBook_ADODotNet/AddressBook_ADODotNet/AddressBookRepo.cs
+++ AddressBook_ADODotNet/AddressBook_ADODotNet/AddressBookRepo.cs
@@ -15,12 +15,55 @@ namespace AddressBook_ADODotNet
             try
             {
                 this.connection.Open();
-                Console.WriteLine("connection established");
+                Console.WriteLine("Connection Established!!");
                 this.connection.Close();
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.StackTrace);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="addressBookModel"></param>
+        /// <returns></returns>
+        public bool AddContactInAddressBook(AddressBookModel addressBookModel)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    SqlCommand cmd = new SqlCommand("SpAddAddressBookDetails", this.connection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@FirstName", addressBookModel.FirstName);
+                    cmd.Parameters.AddWithValue("@LastName", addressBookModel.LastName);
+                    cmd.Parameters.AddWithValue("@Address", addressBookModel.Address);
+                    cmd.Parameters.AddWithValue("@City", addressBookModel.City);
+                    cmd.Parameters.AddWithValue("@State", addressBookModel.State);
+                    cmd.Parameters.AddWithValue("@Zip", addressBookModel.Zip);
+                    cmd.Parameters.AddWithValue("@PhoneNumber", addressBookModel.PhoneNumber);
+                    cmd.Parameters.AddWithValue("@Email", addressBookModel.Email);
+                    cmd.Parameters.AddWithValue("@AddressBookName", addressBookModel.AddressBookName);
+                    cmd.Parameters.AddWithValue("@AddressBookType", addressBookModel.AddressBookType);
+                    this.connection.Open();
+                    var result = cmd.ExecuteNonQuery();
+                    this.connection.Close();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
             }
         }
     }
--- AddressBook_ADODotNet/AddressBook_ADODotNet/Program.cs
+++ AddressBook_ADODotNet/AddressBook_ADODotNet/Program.cs
@@ -9,7 +9,19 @@ namespace AddressBook_ADODotNet
             Console.WriteLine("================= Welcome To Address Book Using ADO Dot Net =======================");
 
             AddressBookRepo addressBookRepo = new AddressBookRepo();
+            AddressBookModel addressBookModel = new AddressBookModel();
+            addressBookModel.FirstName = "Sanju";
+            addressBookModel.LastName = "Jaiswal";
+            addressBookModel.Address = "Madgaon";
+            addressBookModel.City = "Margao";
+            addressBookModel.State = "Goa";
+            addressBookModel.Zip = 413512;
+            addressBookModel.PhoneNumber = 9975776600;
+            addressBookModel.Email = "sanju@gmail.com";
+            addressBookModel.AddressBookName = "Friend address book";
+            addressBookModel.AddressBookType = "Friends";
             addressBookRepo.checkConnection();
+            addressBookRepo.AddContactInAddressBook(addressBookModel);
         }
     }
 }
